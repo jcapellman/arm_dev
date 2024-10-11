56508f7acc48bb0214aa0b75c4fa7db77049c728
Jarred Capellman <jcapellman@hotmail.com>
2024-10-11 16:21:56 -0500

    Change: Updated code to accept filenames

--- npu_test/Program.cs
+++ npu_test/Program.cs
@@ -7,21 +7,32 @@ namespace npu_test
     {
         static void Main(string[] args)
         {
+            if (args.Length < 2)
+            {
+                Console.WriteLine("Model Name and Input Data are required inputs");
+
+                return;
+            }
+
+            var modelName = args[0];
+            var inputFileName = args[1];
+
             // Create Session options
             using var _sessionOptions = new SessionOptions();
-            Dictionary<string, string> config = new Dictionary<string, string> {
+            Dictionary<string, string> config = new()
+            {
                 { "backend_path", "QnnHtp.dll"},
                 { "enable_htp_fp16_precision", "1"}
             };
 
             _sessionOptions.AppendExecutionProvider("QNN", config);
 
-            using var _encoderSession = new InferenceSession(args[0], _sessionOptions);
+            using var _encoderSession = new InferenceSession(modelName, _sessionOptions);
             var inputTensor = new DenseTensor<float>(new float[] { /* input data */ }, new int[] { /* dimensions */ });
             var inputs = new List<NamedOnnxValue> { NamedOnnxValue.CreateFromTensor("input", inputTensor) };
 
-            using var results = session.Run(inputs);
-            var output = results.First().AsTensor<float>().ToArray();
+            using var results = _encoderSession.Run(inputs);
+            var output = results[0].AsTensor<float>().ToArray();
         }
     }
 }
